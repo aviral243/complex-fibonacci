9c58ebebc9e1827534024aea09b3e751dd516254
aviral243 <aviralgangwar24@gmail.com>
2020-04-02 13:38:48 +0530

    checkpoint addition

--- client/src/OtherPage.js
+++ client/src/OtherPage.js
@@ -1,11 +1,11 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default () => {
   return (
     <div>
-      I'm some other page!
-      <Link to="/">Go back home</Link>
+      Im some other page
+      <Link to="/">Go back to home page!</Link>
     </div>
   );
 };
